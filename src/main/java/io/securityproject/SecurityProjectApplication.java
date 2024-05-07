c9d9927a256d311bade2f885ea410866da92d63e
PeachCoolPiece <121959157+PeachCoolPiece@users.noreply.github.com>
2024-05-07 21:58:29 +0900

    feat. 프로젝트 초기세팅

---
+++ src/main/java/io/securityproject/SecurityProjectApplication.java
@@ -0,0 +1,13 @@
+package io.securityproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityProjectApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityProjectApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/io/securityproject/SecurityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.securityproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityProjectApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
