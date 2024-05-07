278cc8632f80352e6fea105e706e8c50aaf3d86f
PeachCoolPiece <121959157+PeachCoolPiece@users.noreply.github.com>
2024-05-07 22:12:15 +0900

    feat. 사용자정의 보안설정 및 기본 사용자구성

---
+++ src/main/java/io/securityproject/controller/HomeController.java
@@ -0,0 +1,28 @@
+package io.securityproject.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping(value="/")
+	public String dashboard() {
+		return "/dashboard";
+	}
+
+	@GetMapping(value="/user")
+	public String user() {
+		return "/user";
+	}
+
+	@GetMapping(value="/manager")
+	public String manager() {
+		return "/manager";
+	}
+
+	@GetMapping(value="/admin")
+	public String admin() {
+		return "/admin";
+	}
+}
---
+++ src/main/java/io/securityproject/security/SecurityConfig.java
@@ -0,0 +1,43 @@
+package io.securityproject.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                
+        ;
+        
+        
+        return http.build();
+    }
+    
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User.withUsername("user")
+                .password("{noop}1234")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user1);
+    }
+}
